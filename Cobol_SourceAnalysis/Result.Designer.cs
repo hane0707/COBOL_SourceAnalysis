af5b56a61914a4686de9d7bbfea22365b926bf6e
hane0707 <41313415+hane0707@users.noreply.github.com>
2020-08-30 22:31:40 +0900

    Add files via upload
    アップロード漏れ

---
+++ Cobol_SourceAnalysis/Result.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Cobol_SourceAnalysis
+{
+    partial class Result
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultFileList = new System.Windows.Forms.ListBox();
+            this.resultOkBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // resultFileList
+            // 
+            this.resultFileList.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.resultFileList.FormattingEnabled = true;
+            this.resultFileList.HorizontalScrollbar = true;
+            this.resultFileList.ItemHeight = 15;
+            this.resultFileList.Location = new System.Drawing.Point(15, 21);
+            this.resultFileList.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.resultFileList.Name = "resultFileList";
+            this.resultFileList.Size = new System.Drawing.Size(529, 169);
+            this.resultFileList.TabIndex = 5;
+            this.resultFileList.TabStop = false;
+            this.resultFileList.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.resultFileList_DrawItem);
+            // 
+            // resultOkBtn
+            // 
+            this.resultOkBtn.AllowDrop = true;
+            this.resultOkBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resultOkBtn.Location = new System.Drawing.Point(457, 196);
+            this.resultOkBtn.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.resultOkBtn.Name = "resultOkBtn";
+            this.resultOkBtn.Size = new System.Drawing.Size(87, 30);
+            this.resultOkBtn.TabIndex = 6;
+            this.resultOkBtn.Text = "OK";
+            this.resultOkBtn.UseVisualStyleBackColor = true;
+            this.resultOkBtn.Click += new System.EventHandler(this.resultOkBtn_Click);
+            // 
+            // Result
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(559, 240);
+            this.Controls.Add(this.resultOkBtn);
+            this.Controls.Add(this.resultFileList);
+            this.Font = new System.Drawing.Font("Meiryo UI", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Result";
+            this.Text = "実行結果";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox resultFileList;
+        private System.Windows.Forms.Button resultOkBtn;
+    }
+}
---
+++ Cobol_SourceAnalysis/Result.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cobol_SourceAnalysis
+{
+    public partial class Result : Form
+    {
+        List<int> ngFileIndex = new List<int>();
+        public Result(List<bool> successFlg, List<string> fileList)
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < successFlg.Count; i++)
+            {
+                resultFileList.Items.Add((successFlg[i]?"ＯＫ　": "ＮＧ　") + fileList[i]);
+
+                if(!successFlg[i])
+                    ngFileIndex.Add(i);
+            }
+        }
+
+        private void resultOkBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void resultFileList_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            Brush brush = new SolidBrush(Color.Black);
+            foreach (var index in ngFileIndex)
+            {
+                if (e.Index == index)
+                    brush = new SolidBrush(Color.Red);
+            }
+
+            e.Graphics.DrawString(resultFileList.Items[e.Index].ToString()
+                , e.Font, brush, e.Bounds, StringFormat.GenericDefault);
+        }
+    }
+}
